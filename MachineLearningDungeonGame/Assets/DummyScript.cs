a1125b8b6dd7d9942f2c8b035d913484fa0aa669
marcis2000 <cisowski@student.agh.edu.pl>
2022-05-23 22:04:39 +0100

    Working on dissolving effect

--- MachineLearningDungeonGame/Assets/DummyScript.cs
+++ MachineLearningDungeonGame/Assets/DummyScript.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class DummyScript : MonoBehaviour
 {
+    [SerializeField] Material dissolveMaterial;
     private Animator _animator;
     private CapsuleCollider dummyCapsuleCollider;
     private BoxCollider dummyBoxCollider;
     private int hitCount;
     private bool isDestroyed = false;
+    private float dissolveAmount;
+
+    private Transform dummyGFX;
+    private SkinnedMeshRenderer renderer;
+    [SerializeField] private float disolveSpeed = 0.001f;
 
     public event EventHandler addToCount;
 
@@ -17,7 +23,12 @@ public class DummyScript : MonoBehaviour
     {
         _animator = this.GetComponent<Animator>();
         dummyCapsuleCollider = this.GetComponent<CapsuleCollider>();
+        dummyGFX = this.transform.GetChild(1);
+        renderer = dummyGFX.GetComponent<SkinnedMeshRenderer>();
+        renderer.enabled = true;
         hitCount = 0;
+        dissolveAmount = -1.25f;
+        Debug.Log(dummyGFX);
     }
 
     void OnCollisionEnter(Collision collision)
@@ -35,8 +46,8 @@ public class DummyScript : MonoBehaviour
                 }
                 else
                 {
-
-                    _animator.SetBool("Destroyed", true);
+                    renderer.material = dissolveMaterial;               
+                    //_animator.SetBool("Destroyed", true);
                     StartCoroutine(DeadCoroutine());
                     isDestroyed = true;
                     addToCount?.Invoke(this, EventArgs.Empty);
@@ -54,8 +65,13 @@ public class DummyScript : MonoBehaviour
     IEnumerator DeadCoroutine()
     {
         yield return new WaitForSeconds(2f);
-        this.gameObject.SetActive(false);
+        //this.gameObject.SetActive(false);
         _animator.SetBool("Dead", false);
+        if(dissolveAmount < 1) 
+        {
+            dissolveAmount = dissolveAmount + Time.deltaTime * disolveSpeed;
+            dissolveMaterial.SetFloat("Dissolve intensity", dissolveAmount);
+        }
 
     }
 }
